4d0367fa1e6f733ef0f0346df92c080914e29f57
Larry <annanshakai@gmail.com>
2024-07-29 05:11:06 +0700

    init commit

---
+++ example-application/src/main/java/com/efasttask/FuncClass.java
@@ -0,0 +1,19 @@
+package com.efasttask;
+
+public class FuncClass {
+    public static void printGoodBye() {
+        System.out.println("Good bye");
+    }
+
+    public static void printHello(int i) {
+        System.out.println("Hello, friend " + i);
+    }
+
+    public static void printSomething() throws InterruptedException {
+        int i = 3600;
+        while (i-- >= 0) {
+            printHello(i);
+            Thread.sleep(1000);
+        }
+    }
+}
---
+++ example-application/src/main/java/com/efasttask/Main.java
@@ -0,0 +1,7 @@
+package com.efasttask;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        FuncClass.printSomething();
+    }
+}
